49941672b2e2d50caae4bd546c9658c15f5382c3
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-19 17:25:15 +0530

    Welcome Employee Payroll Service inn java IO

---
+++ src/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import java.util.*;
+
+public class EmployeePayrollService {
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollDataList) {
+        this.employeePayrollList = employeePayrollDataList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee Id: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        Double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println(employeePayrollList);
+    }
+}
